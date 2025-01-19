733ba9e447a8a38078c5753c875238dbb10835eb
tesfaTMH <hagos.tesfam@gmail.com>
2025-01-19 08:53:19 +0200

    Add dynamic metadata using generateMetadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,18 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/posts";
 
+// dynamic metadata
+
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `Browse all ${numberOfPosts} posts`,
+    description: "Browse all posts",
+  };
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
